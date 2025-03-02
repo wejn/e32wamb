0d9c0453d75e75e05ca8a4c6f30a4b3cdd35ce4d
Michal Jirků <box@wejn.org>
2025-03-02 10:19:30 +0100

    Get rid of STRINGIFY() which was a cringe

--- main/e32wamb.h
+++ main/e32wamb.h
@@ -40,13 +40,10 @@
     .app_device_version = 2, \
 }
 
-#define TOSTRING(x) #x
-#define STRINGIFY(x) TOSTRING(x)
-
 #define MY_LIGHT_CONFIG() { \
     .manufacturer_name = "\x08""wejn.org", \
     .model_identifier = "\x07""e32wamb", \
-    .date_code = "\x0e" STRINGIFY(BUILD_DATE_CODE), \
+    .date_code = "\x0e" BUILD_DATE_CODE, \
     /* FIXME: fill in .build_id to GIT_FULL_REV_ID, but it needs sz prefix */ \
     .power_source = 0x01, \
     .onoff = 1, \
