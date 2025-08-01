ba678ce31c169bba54002eed572ec5a59a86d17a
Michal Jirků <box@wejn.org>
2025-08-01 10:20:27 +0200

    Properly enable RF Switch
    It was pointed out (by @obrain17) in #1 that the RF Switch needs
    enabling via pulling GPIO3 low:
    https://wiki.seeedstudio.com/xiao_esp32c6_getting_started/
    (RF Switch section)
    
    Thank you, @obrain17!
    
    Closes #1.

--- main/rfswitch.c
+++ main/rfswitch.c
@@ -12,6 +12,17 @@
 
 static const char *TAG = "RF_SWITCH";
 
+// https://wiki.seeedstudio.com/xiao_esp32c6_getting_started/
+// See the "RF Switch" section there,
+// or https://github.com/wejn/e32wamb/issues/1
+#ifndef RF_SW_ENABLE_GPIO
+#define RF_SW_ENABLE_GPIO GPIO_NUM_3
+#endif
+
+#ifndef RF_SWITCH_GPIO
+#define RF_SWITCH_GPIO -1
+#endif
+
 esp_err_t rf_switch_initialize(bool external) {
   esp_err_t ret = ESP_OK;
 
@@ -20,7 +31,24 @@ esp_err_t rf_switch_initialize(bool external) {
     return ESP_OK;
   }
 
-  ESP_LOGI(TAG, "configuring on gpio %d", RF_SWITCH_GPIO);
+  ESP_LOGI(TAG, "configuring on gpio %d (rf switch enable pin)", RF_SW_ENABLE_GPIO);
+  gpio_config_t enable_conf = {
+    .intr_type = GPIO_INTR_DISABLE,
+    .mode = GPIO_MODE_OUTPUT,
+    .pin_bit_mask = 1ULL<<RF_SW_ENABLE_GPIO,
+    .pull_down_en = GPIO_PULLDOWN_ENABLE,
+    .pull_up_en = GPIO_PULLUP_DISABLE,
+  };
+
+  ret = gpio_config(&enable_conf);
+  if (ret != ESP_OK) {
+    ESP_LOGW(TAG, "can't configure: %s", esp_err_to_name(ret));
+    return ret;
+  }
+
+  // XXX: NOT sleeping here; it'll catch up (the rest of the init also takes time)
+
+  ESP_LOGI(TAG, "configuring on gpio %d (rf switch select pin)", RF_SWITCH_GPIO);
 
   gpio_config_t io_conf = {
     .intr_type = GPIO_INTR_DISABLE,
@@ -40,6 +68,12 @@ esp_err_t rf_switch_initialize(bool external) {
 }
 
 esp_err_t rf_switch_set(bool external) {
+
+  if (RF_SWITCH_GPIO < 0) {
+    ESP_LOGI(TAG, "NOT setting (disabled)");
+    return ESP_OK;
+  }
+
   ESP_LOGI(TAG, "setting to %s antenna", external ? "u.fl" : "built-in");
   esp_err_t ret = gpio_set_level(RF_SWITCH_GPIO, external);
 
